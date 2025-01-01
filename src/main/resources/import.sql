e9c8e24703826178b1d9b7404725f3619a6df764
MatheusFerreiraL <matheusfleandro@gmail.com>
2025-01-01 15:05:51 +0000

    :sparkles: Add dummy data for tables

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,12 @@
-INSERT INTO kitchen (name) VALUES ("Brazilian");
+INSERT INTO kitchen (name) VALUES ("Brazilian"), ("Chinese");
 
-INSERT INTO kitchen (name) VALUES ("Chinese");
+INSERT INTO restaurant (name, shipping_fee, kitchen_id) VALUES ("Musashi", 10.0, 2), ("Abraquebabra", 3.50, 1);
 
-INSERT INTO restaurant (name, shipping_fee, kitchen_id) VALUES ("Musashi", 10.0, 2);
+INSERT INTO state (name) VALUES ('Leinster'), ('Munster');
 
-INSERT INTO restaurant (name, shipping_fee, kitchen_id) VALUES ("Abraquebabra", 3.50, 1);
+INSERT INTO permission (name, description) VALUES ('MANAGE_USERS', 'Permission to manage users'), ('MANAGE_ORDERS', 'Permission to manage orders');
+
+INSERT INTO payment_method (description) VALUES ('Credit Card'), ('Cash');
+
+INSERT INTO city (name, state_id) VALUES ('Dublin', 1), ('Cork', 2);
+
