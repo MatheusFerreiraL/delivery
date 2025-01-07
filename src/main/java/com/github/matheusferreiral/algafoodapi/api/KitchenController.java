8495c4e278d4bcb3a47b5ca19619aa091dfa1609
MatheusFerreiraL <matheusfleandro@gmail.com>
2025-01-07 01:54:32 +0000

    :sparkles: Implement update method for kitchen resource

--- src/main/java/com/github/matheusferreiral/algafoodapi/api/KitchenController.java
+++ src/main/java/com/github/matheusferreiral/algafoodapi/api/KitchenController.java
@@ -3,12 +3,14 @@ package com.github.matheusferreiral.algafoodapi.api;
 import com.github.matheusferreiral.algafoodapi.domain.model.Kitchen;
 import com.github.matheusferreiral.algafoodapi.domain.repository.KitchenRepository;
 import java.util.List;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -38,5 +40,18 @@ public class KitchenController {
   @ResponseStatus(HttpStatus.CREATED)
   public Kitchen add(@RequestBody Kitchen kitchen) {
     return kitchenRepository.save(kitchen);
+  }
+
+  @PutMapping("/{kitchenId}")
+  public ResponseEntity<?> update(@PathVariable Long kitchenId, @RequestBody Kitchen kitchen) {
+    Kitchen currentKitchen = kitchenRepository.findById(kitchenId);
+
+    if (currentKitchen == null) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Kitchen not found! :/");
+    }
+    // from the third parameter onwards, are properties that will be ignored on the 'copy & paste'
+    BeanUtils.copyProperties(kitchen, currentKitchen, "id");
+    currentKitchen = kitchenRepository.save(currentKitchen);
+    return ResponseEntity.status(HttpStatus.OK).body(currentKitchen);
   }
 }
