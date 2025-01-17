7f3bfb45d15fc732752ca0eca108fc772eb7f272
MatheusFerreiraL <matheusfleandro@gmail.com>
2025-01-17 00:30:16 +0000

    :sparkles: Implement update method for restaurant

--- src/main/java/com/github/matheusferreiral/algafoodapi/api/RestaurantController.java
+++ src/main/java/com/github/matheusferreiral/algafoodapi/api/RestaurantController.java
@@ -2,14 +2,17 @@ package com.github.matheusferreiral.algafoodapi.api;
 
 import com.github.matheusferreiral.algafoodapi.domain.exception.EntityNotFoundException;
 import com.github.matheusferreiral.algafoodapi.domain.model.Restaurant;
+import com.github.matheusferreiral.algafoodapi.domain.service.KitchenService;
 import com.github.matheusferreiral.algafoodapi.domain.service.RestaurantService;
 import java.util.List;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestaurantController {
 
   @Autowired private RestaurantService restaurantService;
+  @Autowired private KitchenService kitchenService;
 
   @GetMapping
   public List<Restaurant> list() {
@@ -46,6 +50,21 @@ public class RestaurantController {
     } catch (EntityNotFoundException entityNotFoundException) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST)
           .body(entityNotFoundException.getMessage());
+    }
+  }
+
+  @PutMapping("/{restaurantId}")
+  public ResponseEntity<?> update(
+      @PathVariable Long restaurantId, @RequestBody Restaurant restaurant) {
+    try {
+      Restaurant currentRestaurant = restaurantService.findById(restaurantId);
+
+      BeanUtils.copyProperties(restaurant, currentRestaurant, "id");
+
+      currentRestaurant = restaurantService.save(currentRestaurant);
+      return ResponseEntity.status(HttpStatus.OK).body(currentRestaurant);
+    } catch (EntityNotFoundException entityNotFoundException) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(entityNotFoundException.getMessage());
     }
   }
 }
