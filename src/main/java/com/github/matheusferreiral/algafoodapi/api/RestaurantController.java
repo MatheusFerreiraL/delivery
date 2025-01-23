bd46845d6b631977d14cc2a5f8cd1d89b8742c07
MatheusFerreiraL <matheusfleandro@gmail.com>
2025-01-23 00:05:53 +0000

    :sparkles: Implement partial update for restaurant

--- src/main/java/com/github/matheusferreiral/algafoodapi/api/RestaurantController.java
+++ src/main/java/com/github/matheusferreiral/algafoodapi/api/RestaurantController.java
@@ -1,16 +1,21 @@
 package com.github.matheusferreiral.algafoodapi.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.matheusferreiral.algafoodapi.domain.exception.EntityNotFoundException;
 import com.github.matheusferreiral.algafoodapi.domain.model.Restaurant;
 import com.github.matheusferreiral.algafoodapi.domain.service.RestaurantService;
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -66,6 +71,35 @@ public class RestaurantController {
     } catch (EntityNotFoundException | DataIntegrityViolationException entityNotFoundException) {
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body(entityNotFoundException.getMessage());
     }
+  }
+
+  @PatchMapping("/{restaurantId}")
+  public ResponseEntity<?> partialUpdate(
+      @PathVariable Long restaurantId, @RequestBody Map<String, Object> fields) {
+    try {
+      Restaurant currentRestaurant = restaurantService.findById(restaurantId);
+      merge(fields, currentRestaurant);
+      return update(restaurantId, currentRestaurant);
+    } catch (EntityNotFoundException entityNotFoundException) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(entityNotFoundException.getMessage());
+    }
+  }
+
+  private static void merge(Map<String, Object> originFields, Restaurant finalRestaurant) {
+    // The object mapper is responsible for dealing with the conversions of all the fields (data
+    // types)
+    ObjectMapper objectMapper = new ObjectMapper();
+    Restaurant originRestaurant = objectMapper.convertValue(originFields, Restaurant.class);
+
+    originFields.forEach(
+        (propertyName, propertyValue) -> {
+          Field field = ReflectionUtils.findField(Restaurant.class, propertyName);
+          field.setAccessible(true);
+
+          Object newValue = ReflectionUtils.getField(field, originRestaurant);
+
+          ReflectionUtils.setField(field, finalRestaurant, newValue);
+        });
   }
 
   @DeleteMapping("/{restaurantId}")
