1f91ef35c6585ae04a8e25e8c1881057cd4077ca
MatheusFerreiraL <matheusfleandro@gmail.com>
2025-04-12 09:59:25 +0100

    chore: relocate data with flyway

--- src/main/resources/db/migration/V003__create-table-state.sql
+++ src/main/resources/db/migration/V003__create-table-state.sql
@@ -1,19 +1,26 @@
-CREATE TABLE state(
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE state
+(
+    id   BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(80) NOT NULL
 ) engine=InnoDB DEFAULT charset=utf8;
 
-## VAI PEGAR OS DADOS QUE JA ESTAO NA TABELA DE CIDADE E VAI PASSAR PARA A TABELA DE ESTADO
-INSERT INTO state (name) SELECT DISTINCT state_name FROM city;
+##
+VAI PEGAR OS DADOS QUE JA ESTAO NA TABELA DE CIDADE E VAI PASSAR PARA A TABELA DE ESTADO
+INSERT INTO state (name)
+SELECT DISTINCT state_name
+FROM city;
 
-ALTER TABLE city ADD COLUMN  state_id BIGINT NOT NULL;
+ALTER TABLE city
+    ADD COLUMN state_id BIGINT NOT NULL;
 
-UPDATE city c SET c.state_id =
-    (SELECT s.id FROM state s WHERE s.name = c.state_name);
+UPDATE city c
+SET c.state_id =
+            (SELECT s.id FROM state s WHERE s.name = c.state_name);
 
-ALTER TABLE city ADD CONSTRAINT fk_city_state
-FOREIGN KEY (state_id) REFERENCES state (id);
+ALTER TABLE city
+    ADD CONSTRAINT fk_city_state
+        FOREIGN KEY (state_id) REFERENCES state (id);
 
 ALTER TABLE city DROP COLUMN state_name;
 
-ALTER TABLE city CHANGE city_name name VARCHAR(80) NOT NULL;
+ALTER TABLE city CHANGE city_name name VARCHAR (80) NOT NULL;
